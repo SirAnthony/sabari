9ab8244aeb194eecb528c5fcd1653a2e897519e2
Sir Anthony <anthony@adsorbtion.org>
2014-09-14 20:09:14 +0600

    Node version

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var nunjucks = require('nunjucks')
+var express = require('express')
+var i18n = require('i18n-abide')
+var settings = require('./settings.json')
+var sabari = require('./lib/sabari.js')
+var app = express();
+
+// Localization
+app.use(i18n.abide({
+	supported_languages: ['en_US', 'ru',],
+	default_lang: "en_US",
+	translation_directory: "templates/static/i18n",
+}))
+
+// Templates setup
+var env = nunjucks.configure('templates', {autoescape: true, express: app})
+
+// Routes setup
+sabari.init(app)
+
+app.listen(settings.port);
+console.log('Server started on port %s', settings.port);
--- viewer-wsgi.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import wsgiref.simple_server
-from libviewer import serve
-
-server = wsgiref.simple_server.make_server('', 8000, serve)
-print "Server listening on port %d" % 8000
-server.serve_forever()
